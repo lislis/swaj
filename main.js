df520fc6736a01ef42478f13f01c8e0abd3a106c
Lisa P <mail@lislis.de>
2015-11-20 17:52:52 +0100

    init

---
+++ main.js
@@ -0,0 +1,18 @@
+// decide on position of shark
+// calc projectile trajectory
+// -> from center of shark
+// indicate that somehow
+// AI for beach bar
+// timer
+
+
+// assets
+// Starting Screen / Menu
+// Shark 
+//    normal
+//    spitting
+// Beach Bar
+//    empty
+//    1/4, 1/2, 3/4 and full
+// Humans
+// Ending Screen
